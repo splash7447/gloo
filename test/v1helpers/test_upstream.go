39280142846bd53fcbfb84e16478840ea5603a09
Sam Heilbron <samheilbron@gmail.com>
2020-11-18 15:42:32 -0500

    newTestUpstream should be configured with panic mode disabled

--- test/v1helpers/test_upstream.go
+++ test/v1helpers/test_upstream.go
@@ -114,6 +114,12 @@ func newTestUpstream(addr string, ports []uint32, responses <-chan *ReceivedRequ
 				Hosts: hosts,
 			},
 		},
+		LoadBalancerConfig: &gloov1.LoadBalancerConfig{
+			// Default test upstreams to disable panic mode
+			HealthyPanicThreshold: &types.DoubleValue{
+				Value: 0.0,
+			},
+		},
 	}
 
 	return &TestUpstream{
